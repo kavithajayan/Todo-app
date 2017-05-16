6d40196e50ee58efdcf62b9c491e8444a37ac14b
divya2012 <divyasarojam@gmail.com>
2017-05-16 15:37:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 if ('serviceWorker' in navigator) {
-     navigator.serviceWorker
-             .register('./service-worker.js')
-             .then(function() 
-                    { console.log('Service Worker Registered'); 
-             });
-}
+  navigator.serviceWorker.register('./service-worker.js', {scope: 'sw-test'}).then(function(registration) {
+    // registration worked
+    console.log('Service Worker Registered');
+    button.onclick = function() {
+      registration.update();
+    }
+  }).catch(function(error) {
+    // registration failed
+    console.log('Registration failed with ' + error);
+  });
+};
